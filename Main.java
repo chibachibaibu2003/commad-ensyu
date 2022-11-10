dc6e4ed0d9dc21093f78b4ef787b53b7f8a0398c
c.ibuki <ibuchan1122@icloud.com>
2022-11-10 10:49:41 +0900

    Main.javaを二人分表示するように修正しました。

--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@ public class Main{
   public static void main(String[] args){
 	  Scanner sc=new Scanner(System.in);
 	  double height;
+	  double height2;
 	  int weight;
-          System.out.println("身長を入力してください。(Mで入力してください。)");
+	  int weight2;
+          System.out.println("一人目の身長を入力してください。(Mで入力してください。)");
           height=sc.nextDouble();
-          System.out.println("体重を入力してください。");
+	  System.out.println("二人目の身長を入力してください(Mで入力してください)");
+	  height2=sc.nextDouble();
+          System.out.println("一人目の体重を入力してください。");
           weight=sc.nextInt();
+	  System.out.println("二人目の体重を入力して下さい");
+	  weight2=sc.nextInt();
           double bmi=weight/(height*height);
-          System.out.println("あなたのBMIは"+(Math.floor(bmi*100)/100)+"です。");
+	  double bmi2=weight2/(height2*height2);
+          System.out.println("一人目ののBMIは"+(Math.floor(bmi*100)/100)+"です。");
+	  System.out.println("二人目のBMIは"+(Math.floor(bmi2*100)/100)+"です。");
   }
 }
